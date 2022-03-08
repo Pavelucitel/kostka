b06b2b1380ee00e5ad2036b06067be6618d6f3e5
Pavelucitel <100760627+Pavelucitel@users.noreply.github.com>
2022-03-08 12:06:14 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(neuspech)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(body)
+})
+input.onGesture(Gesture.Shake, function () {
+    cislo = 0
+    cislo = randint(1, 6)
+    if (cislo == 6) {
+        basic.showNumber(cislo)
+        basic.pause(100)
+        basic.showIcon(IconNames.Yes)
+        body += 1
+    } else {
+        basic.showNumber(cislo)
+        neuspech += 1
+        basic.pause(100)
+        basic.showString("znova")
+    }
+})
+let cislo = 0
+let neuspech = 0
+let body = 0
+body = 0
+neuspech = 0
+basic.forever(function () {
+	
+})
